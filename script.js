b4793275dc4e528b2fd970601226b60d754f5f24
DevangiChauhan <123537107+DevangiChauhan@users.noreply.github.com>
2024-03-22 14:54:38 +0530

    Add files via upload
    Updated files

--- script.js
+++ script.js
@@ -56,3 +56,12 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+const checkbox = document.querySelector('.my-form input[type="checkbox"]');
+const btns = document.querySelectorAll(".my-form button");
+
+checkbox.addEventListener("change", function() {
+  const checked = this.checked;
+  for (const btn of btns) {
+    checked ? (btn.disabled = false) : (btn.disabled = true);
+  }
+});
